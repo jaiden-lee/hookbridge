b54e04e25055151d4e15309646c3d72ff73bcc17
jaiden-lee <jaidenwlee@gmail.com>
2026-02-01 14:18:01 -0500

    protobuf definition for tunnel

---
+++ cmd/tunnel/main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	// primary owner is sent as part of the forwarded request as a tag
+	// when client receives, if it has that as primary owner, it will forward automatically
+
+}
